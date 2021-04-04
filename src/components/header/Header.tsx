49aa99ff01f8b25137dfe5c450ee09b4ef66815e
Calin Biberea <calinbiberea@gmail.com>
2021-04-04 18:00:17 +0100

    Add template

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+const Header: React.FC = () => {
+  return (
+    <header id="home">
+      <nav id="nav-wrap">
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+          Show navigation
+        </a>
+        <a className="mobile-btn" href="#home" title="Hide navigation">
+          Hide navigation
+        </a>
+
+        <ul id="nav" className="nav">
+          <li className="current">
+            <a className="smoothscroll" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#resume">
+              Resume
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#portfolio">
+              Works
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#testimonials">
+              Testimonials
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="row banner">
+        <div className="banner-text">
+          <h1 className="responsive-headline">I am Calin.</h1>
+          <h3>
+            I am a poop based <span>poopsland</span>. Poop.
+          </h3>
+          <hr />
+          <ul className="social">networks</ul>
+        </div>
+      </div>
+
+      <p className="scrolldown">
+        <a className="smoothscroll" href="#about">
+          <i className="icon-down-circle" />
+        </a>
+      </p>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/portfolio/Portfolio.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Portfolio: React.FC = () => {
+  return null;
+};
+
+export default Portfolio;
