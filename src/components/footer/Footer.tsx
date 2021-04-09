d623184c967e9747713f2cc4bd12a2363ee1cf27
Calin Biberea <calinbiberea@gmail.com>
2021-04-09 15:30:29 +0100

    Add Footer.tsx

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { animateScroll as scroll } from "react-scroll";
 
 const Footer: React.FC = () => {
-  return null;
+  return (
+    <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="footerSocial">
+            <li>
+              <a href="https://www.facebook.com/calinbiberea">
+                <i className="fa fa-facebook" />
+              </a>
+            </li>
+            <li>
+              <a href="https://github.com/calinbiberea">
+                <i className="fa fa-github" />
+              </a>
+            </li>
+            <li>
+              <a href="https://www.linkedin.com/in/calinbiberea/">
+                <i className="fa fa-linkedin" />
+              </a>
+            </li>
+          </ul>
+
+          <ul className="copyright">
+            <li>&copy; Copyright 2021 Calin Biberea</li>
+          </ul>
+        </div>
+        <div id="toTop" className="scrollup" onClick={scroll.scrollToTop} aria-hidden>
+          {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+          <a>
+            <i className="icon-up-open" />
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
